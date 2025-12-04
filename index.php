b3a692a01e5cf48cadd0aedb2a47b5f98e5357c7
DiscountSponge <156786512+DiscountSponge@users.noreply.github.com>
2025-12-04 12:46:40 +0000

    dompdf

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
-$view = new stdClass();
-$view->pageTitle = 'Pet Watch';
+require __DIR__ . '/vendor/autoload.php';
 
-require_once('Views/index.phtml');
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+$name = "FUCK THIS SHIIIIIT";
+
+$options = new Options();
+$options->setChroot(__DIR__);
+$dompdf = new Dompdf();
+$dompdf->setBasePath(__DIR__ ."/Views/css/bootstrap.css");
+$html = file_get_contents(__DIR__ ."/Views/template/template.phtml");
+$html = str_replace("{{name}}", $name, $html);
+$dompdf->loadHtml($html);
+$dompdf->setPaper('A4', 'portrait');
+$dompdf->render();
+$dompdf->stream("invoice.pdf", ["Attachment" => false]);

722353b4c6509d6b5756ae3d806a41a26b4383fd
Riadh <ryadmk88@gmail.com>
2025-12-04 12:43:44 +0000

    updated logic made empty pages and class and test it

---
+++ page2.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page 2</title>
+</head>
+<body>
+
+<h1>Page 2 (Empty Base Page)</h1>
+<p>This page is intentionally empty and will be filled later.</p>
+
+</body>
+</html>

bd237e837638681da02e8a0ede458976d92fac59
DiscountSponge <156786512+DiscountSponge@users.noreply.github.com>
2025-12-11 19:40:01 +0000

    2 different location things innit

--- Model/StaffData.php
+++ Model/StaffData.php
@@ -3,23 +3,17 @@
 class StaffData
 {
 
-    protected $_id, $_user_id, $_skills, $current_status, $last_latitude, $last_longitude, $last_updated_at;
-    /**
-     * Constructor for the PetData class.
-     *
-     * @param array $dbRow //Modifies from students version we were given
-     */
+    public $_id, $_user_id, $_skills, $current_status, $last_latitude, $last_longitude, $last_updated_at;
+
     public function __construct($dbRow)
     {
-        $this->_id = $dbRow["id"];
-        $this->_user_id = $dbRow["user_id"];
-        $this->_skills = $dbRow["skills"];
-        $this->current_status = $dbRow["current_status"];
-        $this->last_latitude = $dbRow["last_latitude"];
-        $this->last_longitude = $dbRow["last_longitude"];
-        $this->last_updated_at = $dbRow["last_updated_at"];
-
-
+        $this->_id = $dbRow["id"] ?? null;
+        $this->_user_id = $dbRow["user_id"] ?? null;
+        $this->_skills = $dbRow["skills"] ?? null;
+        $this->current_status = $dbRow["current_status"] ?? null;
+        $this->last_latitude = $dbRow["last_latitude"] ?? null;
+        $this->last_longitude = $dbRow["last_longitude"] ?? null;
+        $this->last_updated_at = $dbRow["last_updated_at"] ?? null;
     }
 
     public function getId(): mixed
